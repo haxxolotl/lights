a409d2b18cd06eadf67d80c898946456d1d58b3e
Haxolotl <liuenting990305@gmail.com>
2022-10-23 19:38:01 -0700

    compile

--- lights.ino
+++ lights.ino
@@ -1,5 +1,5 @@
 #include <FastLED.h>
-
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
 int brightness = 8;
 int counter = 0;
 int current_program = 0;
@@ -19,7 +19,7 @@ struct PhysicalSegment {
 #define num_dual_segment_leds 356
 #define num_fairy_segment_leds 400
 CRGB strip_segment_leds[num_strip_segment_leds];
-CRGB dual_segment_leds[dual_segment_leds];
+CRGB dual_segment_leds[num_dual_segment_leds];
 CRGB fairy_segment_leds[num_fairy_segment_leds];
 
 PhysicalSegment strip_segment = {num_strip_segment_leds, strip_segment_leds, &FastLED.addLeds<WS2811, 7, BRG>(strip_segment_leds, num_strip_segment_leds)};   // strip 1*100
@@ -62,7 +62,7 @@ LogicalSegment fairy_segment_6 = {&fairy_segment, 377, 400, false};
 // groups can have strip combination that overlap other groups
 struct LogicalSegmentGroup {
   LogicalSegment **logical_segments;
-  int segments;
+  int num_segments;
 };
 LogicalSegment *strip_segments[6] = {&strip_segment_up, &strip_segment_over, &dual_segment_right_corner, &dual_segment_back_ceiling, &dual_segment_left_ceiling, &dual_segment_front_ceiling};
 LogicalSegment *strip_ceiling_segments[5] = {&strip_segment_over, &dual_segment_right_corner, &dual_segment_back_ceiling, &dual_segment_left_ceiling, &dual_segment_front_ceiling};
@@ -96,7 +96,7 @@ LogicalSegmentGroup all_group = {all_segments, 15};
 typedef void (*LightFunction)(CRGB *leds, int count);
 typedef void (*SequenceGroupSideEffect)();
 
-// A program is looping sequence groups, each group is made of a list of sequences
+// A program is looping sequence groups, each group is made of a list of sequences, and an optional side effect to modify any globals
 // a sequence is a light function applied to some range of logical RGB
 
 // apply this light function to either a segment or a group, other one is nullptr
@@ -117,126 +117,38 @@ struct LightSequenceGroup {
 struct LightProgram {
   LightSequenceGroup *light_sequence_groups;
   int num_groups;
-};
-
-void black(CRGB *leds, int count) { fill_solid(leds, count, CRGB::Black); }
-void rainbow(CRGB *leds, int count) { fill_rainbow(leds, count, counter, program_parameter_x * 2 % 10); }
-
-LightSequence reset_sequence = {&black, nullptr, nullptr};
-LightSequence rainbow_sequence = {&rainbow, nullptr, nullptr};
-LightSequenceGroup reset_group = {&reset_sequence, 1, 1000, []() {}};
-LightSequenceGroup rainbow_group = {&rainbow_sequence, 1, 1000, []() { counter += program_parameter_y; }};
-
-#define num_programs 5
-LightProgram programs[num_programs] = {
-    {&reset_group, 1},
-    {&rainbow_group, 1},
+  const char *name;
 };
 
 #pragma endregion LightProgram
 
-#pragma region Button
+#pragma region simple_programs
 
-typedef void (*ButtonCallback)();
-struct Button {
-  int pin;
-  bool pressed;
-  ButtonCallback callback;
-};
+void black(CRGB *leds, int count) { fill_solid(leds, count, CRGB::Black); }
+LightSequence reset_sequence = {&black, nullptr, nullptr};
+LightSequenceGroup reset_group = {&reset_sequence, 1, 1000, []() {}};
 
-#define num_buttons 4
-Button buttons[4] = {
-    {3, false,
-     []() {
-       // next brightness setting
-       if (brightness == 0)
-         brightness = 1;
-       else
-         brightness *= 2;
-       if (brightness > 128) brightness = 0;
-     }},
-    {2, false,
-     []() {  // next lighting program
-       current_program = (num_programs + current_program + 1) % num_programs;
-       program_parameter_x = 0;
-       program_parameter_y = 0;
-       counter = 0;
-     }},
-    {1, false, []() { program_parameter_x++; }},
-    {0, false, []() { program_parameter_y++; }},
-};
+void rainbow(CRGB *leds, int count) { fill_rainbow(leds, count, counter, program_parameter_x * 2 % 10); }
+LightSequence rainbow_sequence = {&rainbow, nullptr, nullptr};
+LightSequenceGroup rainbow_group = {&rainbow_sequence, 1, 1000, []() { counter += program_parameter_y % 6; }};
 
-#pragma endregion Button
+void red(CRGB *leds, int count) { fill_solid(leds, count, CRGB::Red); }
+void green(CRGB *leds, int count) { fill_solid(leds, count, CRGB::Green); }
+void blue(CRGB *leds, int count) { fill_solid(leds, count, CRGB::Blue); }
+void white(CRGB *leds, int count) { fill_solid(leds, count, CRGB::White); }
+LightSequence red_sequence = {&red, nullptr, nullptr};
+LightSequence green_sequence = {&green, nullptr, nullptr};
+LightSequence blue_sequence = {&blue, nullptr, nullptr};
+LightSequence white_sequence = {&white, nullptr, nullptr};
+LightSequenceGroup red_group = {&red_sequence, 1, 500, []() {}};
+LightSequenceGroup green_group = {&green_sequence, 1, 500, []() {}};
+LightSequenceGroup blue_group = {&blue_sequence, 1, 500, []() {}};
+LightSequenceGroup white_group = {&white_sequence, 1, 500, []() {}};
+LightSequenceGroup *rgbw_groups[] = {&red_group, &green_group, &blue_group, &white_group};
 
-void setup() {
-  delay(2000);  // sanity delay
-  for (int i = 0; i < num_buttons; i++) {
-    pinMode(buttons[i].pin, INPUT_PULLUP);
-  }
-}
+#pragma endregion simple_programs
 
-void tick() {
-  LightProgram &program = programs[current_program];
-  LightSequenceGroup *group = nullptr;
-  int program_time = 0;
-  for (int i = 0; i < program.num_groups; i++) {
-    program_time += program.light_sequence_groups[i].duration;
-    if (counter <= program_time) {
-      group = &program.light_sequence_groups[i];
-    }
-  }
-  group->side_effect();
-  for (int i = 0; i < group->num_sequences; i++) {
-    LightSequence &sequence = group->sequences[i];
-    if (sequence.logical_segment != nullptr) {
-    } else if (sequence.logical_segment_group != nullptr) {
-    } else {
-      for (int i = 0; i < num_physical_segments; i++) {
-        sequence.light_function(physical_segments->leds, physical_segments->count);
-      }
-    }
-  }
-
-  if (counter > program_time) {
-    counter = 0;
-  }
-
-  for (size_t i = 0; i < num_physical_segments; i++) {
-    physical_segments[i].controller->showLeds(brightness);
-  }
-  for (size_t i = 0; i < num_buttons; i++) {
-    int c = digitalRead(buttons[i].pin);
-    if (c == HIGH && !buttons[i].pressed) {
-      buttons[i].callback();
-      buttons[i].pressed = true;
-    }
-    if (c == LOW) {
-      buttons[i].pressed = false;
-    }
-  }
-  counter++;
-}
-
-void debug() {
-  if (hue < 64) {
-    fill_solid(dual, num_dual, CRGB::Blue);
-    fill_solid(up_strip, num_up_strip, CRGB::Blue);
-    fill_solid(middle, num_middle, CRGB::Blue);
-  } else if (hue < 128) {
-    fill_solid(dual, num_dual, CRGB::Red);
-    fill_solid(up_strip, num_up_strip, CRGB::Red);
-    fill_solid(middle, num_middle, CRGB::Red);
-  } else if (hue < 196) {
-    fill_solid(dual, num_dual, CRGB::Green);
-    fill_solid(up_strip, num_up_strip, CRGB::Green);
-    fill_solid(middle, num_middle, CRGB::Green);
-  } else {
-    fill_solid(dual, num_dual, CRGB::White);
-    fill_solid(up_strip, num_up_strip, CRGB::White);
-    fill_solid(middle, num_middle, CRGB::White);
-  }
-  hue += 3;
-}
+#pragma region pacifica
 
 // Add one layer of waves into the led array
 void pacifica_one_layer(CRGB *leds, int count, CRGBPalette16 &p, uint16_t cistart, uint16_t wavescale, uint8_t bri, uint16_t ioff) {
@@ -302,7 +214,7 @@ CRGBPalette16 pacifica_palette_2 = {0x000507, 0x000409, 0x00030B, 0x00030D, 0x00
 CRGBPalette16 pacifica_palette_3 = {0x000208, 0x00030E, 0x000514, 0x00061A, 0x000820, 0x000927, 0x000B2D, 0x000C33,
                                     0x000E39, 0x001040, 0x001450, 0x001860, 0x001C70, 0x002080, 0x1040BF, 0x2060FF};
 
-void pacifica_loop(CRGB *leds, int count) {
+void pacifica(CRGB *leds, int count) {
   // Increment the four "color index start" counters, one for each wave layer.
   // Each is incremented at a different speed, and the speeds vary over time.
   static uint16_t sCIStart1, sCIStart2, sCIStart3, sCIStart4;
@@ -337,51 +249,53 @@ void pacifica_loop(CRGB *leds, int count) {
   pacifica_deepen_colors(leds, count);
 }
 
-void pacifica() {
-  pacifica_loop(dual, num_dual);
-  pacifica_loop(up_strip, num_up_strip);
-  pacifica_loop(middle, num_middle);
-}
+LightSequence pacifica_sequence = {&pacifica, nullptr, nullptr};
+LightSequenceGroup pacifica_group = {&pacifica_sequence, 1, 1000, []() { counter += program_parameter_y; }};
+#pragma endregion pacifica
 
-void rainbow_glitter() {
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(dual, num_dual, 80);
-  addGlitter(up_strip, num_up_strip, 80);
-  addGlitter(middle, num_middle, 80);
-}
-
-void addGlitter(CRGB *leds, int count, fract8 chanceOfGlitter) {
-  if (random8() < chanceOfGlitter) {
+void rainbow_glitter(CRGB *leds, int count) {
+  rainbow(leds, count);
+  if (random8() < 80) {
     leds[random16(count)] += CRGB::White;
   }
 }
+LightSequence rainbow_glitter_sequence = {&rainbow_glitter, nullptr, nullptr};
+LightSequenceGroup rainbow_glitter_group = {&rainbow_glitter_sequence, 1, 1000, []() { counter += program_parameter_y % 6; }};
 
-void confetti_loop(CRGB *leds, int count) {
+void confetti(CRGB *leds, int count) {
   // random colored speckles that blink in and fade smoothly
   fadeToBlackBy(leds, count, 10);
   int pos = random16(count);
-  leds[pos] += CHSV(hue + random8(64), 200, 255);
+  leds[pos] += CHSV(counter + random8(64), 200 + (program_parameter_x % 5) * 10, 255);
 }
 
-void sinelon_loop(CRGB *leds, int count) {
+LightSequence confetti_sequence = {&confetti, nullptr, nullptr};
+LightSequenceGroup confetti_group = {&confetti_sequence, 1, 1000, []() { counter += program_parameter_y % 6; }};
+
+void sinelon(CRGB *leds, int count) {
   // a colored dot sweeping back and forth, with fading trails
   fadeToBlackBy(leds, count, 20);
-  int pos = beatsin16(13, 0, count - 1);
-  leds[pos] += CHSV(hue, 255, 192);
+  int pos = beatsin16(13 + (program_parameter_x % 5) * 2 - 5, 0, count - 1);
+  leds[pos] += CHSV(counter, 255, 192);
 }
 
-void bpm_loop(CRGB *leds, int count) {
+LightSequence sinelon_sequence = {&sinelon, nullptr, nullptr};
+LightSequenceGroup sinelon_group = {&sinelon_sequence, 1, 1000, []() { counter += program_parameter_y % 6; }};
+
+void bpm(CRGB *leds, int count) {
   // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
+  uint8_t BeatsPerMinute = +(program_parameter_x % 8) * 10 + 30;
   CRGBPalette16 palette = PartyColors_p;
   uint8_t beat = beatsin8(BeatsPerMinute, 64, 255);
   for (int i = 0; i < count; i++) {  // 9948
-    leds[i] = ColorFromPalette(palette, hue + (i * 2), beat - hue + (i * 10));
+    leds[i] = ColorFromPalette(palette, counter + (i * 2), beat - counter + (i * 10));
   }
 }
 
-void juggle_loop(CRGB *leds, int count) {
+LightSequence bpm_sequence = {&bpm, nullptr, nullptr};
+LightSequenceGroup bpm_group = {&bpm_sequence, 1, 1000, []() { counter += program_parameter_y % 6; }};
+
+void juggle(CRGB *leds, int count) {
   // eight colored dots, weaving in and out of sync with each other
   fadeToBlackBy(leds, count, 20);
   uint8_t dothue = 0;
@@ -391,58 +305,116 @@ void juggle_loop(CRGB *leds, int count) {
   }
 }
 
-void confetti() {
-  confetti_loop(dual, num_dual);
-  confetti_loop(up_strip, num_up_strip);
-  confetti_loop(middle, num_middle);
+LightSequence juggle_sequence = {&juggle, nullptr, nullptr};
+LightSequenceGroup juggle_group = {&juggle_sequence, 1, 1000, []() { counter += program_parameter_y % 6; }};
+
+#define num_programs 5
+LightProgram programs[num_programs] = {
+    {&reset_group, 1, "reset"}, {&rainbow_group, 1, "rainbow"}, {rgbw_groups[0], 4, "rgbw"}, {&pacifica_group, 1, "pacifica"}, {&rainbow_glitter_group, 1, "glitter"},
+};
+
+#pragma region Button
+
+typedef void (*ButtonCallback)();
+struct Button {
+  int pin;
+  bool pressed;
+  ButtonCallback callback;
+};
+
+#define num_buttons 4
+Button buttons[4] = {
+    {3, false,
+     []() {
+       // next brightness setting
+       if (brightness == 0)
+         brightness = 1;
+       else
+         brightness *= 2;
+       if (brightness > 128) brightness = 0;
+     }},
+    {2, false,
+     []() {  // next lighting program
+       current_program = (num_programs + current_program + 1) % num_programs;
+       program_parameter_x = 0;
+       program_parameter_y = 0;
+       counter = 0;
+     }},
+    {1, false, []() { program_parameter_x++; }},
+    {0, false, []() { program_parameter_y++; }},
+};
+
+#pragma endregion Button
+
+#pragma region main
+
+void setup() {
+  delay(2000);  // sanity delay
+  Serial.print("start");
+  // for (int i = 0; i < num_buttons; i++) {
+  //   Serial.print("button" + String(i));
+  //   pinMode(buttons[i].pin, INPUT_PULLUP);
+  // }
 }
 
-void sinelon() {
-  sinelon_loop(dual, num_dual);
-  sinelon_loop(up_strip, num_up_strip);
-  sinelon_loop(middle, num_middle);
-}
-
-void bpm() {
-  bpm_loop(dual, num_dual);
-  bpm_loop(up_strip, num_up_strip);
-  bpm_loop(middle, num_middle);
-}
-
-void juggle() {
-  juggle_loop(dual, num_dual);
-  juggle_loop(up_strip, num_up_strip);
-  juggle_loop(middle, num_middle);
+void process_logical_segment(LightFunction light_function, LogicalSegment *logical_segment) {
+  light_function(logical_segment->physical_segment->leds + logical_segment->start, logical_segment->end - logical_segment->start);
+  if (logical_segment->pixel_shift) {
+    for (int i = logical_segment->start; i < logical_segment->end; i++) {
+      CRGB &color = logical_segment->physical_segment->leds[i];
+      uint8_t temp = color.r;
+      color.r = color.g;
+      color.g = color.b;
+      color.b = temp;
+    }
+  }
 }
 
 void loop() {
-  funcs[mode]();
-  if (cycle == 1) {
-    fill_solid(middle, num_middle, CRGB::Black);
-    fill_solid(dual, num_dual, CRGB::Black);
-  }
-  if (cycle == 2) {
-    fill_solid(dual, num_dual, CRGB::Black);
-    fill_solid(up_strip, num_up_strip, CRGB::Black);
-  }
-  if (cycle == 3) {
-    fill_solid(middle, num_middle, CRGB::Black);
-    fill_solid(up_strip, num_up_strip, CRGB::Black);
-  }
-  if (cycle == 4) {
-    fill_solid(dual, num_dual, CRGB::Black);
-    fill_solid(up_strip, num_up_strip, CRGB::Black);
-    fill_solid(middle, num_middle, CRGB::Black);
-  }
-  present();
-  // switch (mode) {
-  //   case 0:
-  //     present(40);
-  //     break;
-  //   default:
-  //     break;
+  Serial.print("loop");
+  // LightProgram &program = programs[current_program];
+  // Serial.print(program.name);
+  // LightSequenceGroup *group = nullptr;
+  // int program_time = 0;
+  // for (int i = 0; i < program.num_groups; i++) {
+  //   program_time += program.light_sequence_groups[i].duration;
+  //   if (counter <= program_time) {
+  //     group = &program.light_sequence_groups[i];
+  //     Serial.print("group");
+  //   }
+  // }
+  // group->side_effect();
+  // for (int i = 0; i < group->num_sequences; i++) {
+  //   LightSequence &sequence = group->sequences[i];
+  //   if (sequence.logical_segment != nullptr) {
+  //   } else if (sequence.logical_segment_group != nullptr) {
+  //     process_logical_segment(sequence.light_function, sequence.logical_segment);
+  //   } else {
+  //     for (int i = 0; i < sequence.logical_segment_group->num_segments; i++) {
+  //       process_logical_segment(sequence.light_function, sequence.logical_segment_group->logical_segments[i]);
+  //     }
+  //   }
   // }
 
-  hue++;
-  hue %= 256;
+  // if (counter > program_time) {
+  //   counter = 0;
+  // }
+
+  // for (size_t i = 0; i < num_physical_segments; i++) {
+  //   physical_segments[i].controller->showLeds(brightness);
+  // }
+  // for (size_t i = 0; i < num_buttons; i++) {
+  //   int c = digitalRead(buttons[i].pin);
+  //   if (c == HIGH && !buttons[i].pressed) {
+  //     buttons[i].callback();
+  //     buttons[i].pressed = true;
+  //   }
+  //   if (c == LOW) {
+  //     buttons[i].pressed = false;
+  //   }
+  // }
+  // counter++;
+  delay(100);
 }
+
+#pragma endregion main
